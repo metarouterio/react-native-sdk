e228963caabfbbc5ba21d04372c1f861f88b556a
Christopher Houdlette <choudlette@gmail.com>
2025-07-16 17:28:47 -0600

    chore: fixing react-native-config

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    dependencies: {
-      '@segment/analytics-react-native': {},
-      '@segment/sovran-react-native': {},
-      'react-native-get-random-values': {},
-    },
-  };
+  dependencies: {
+    '@segment/analytics-react-native': {},
+    '@segment/sovran-react-native': {},
+    'react-native-get-random-values': {},
+  },
+  platforms: {
+    ios: {},
+    android: {},
+  },
+};
